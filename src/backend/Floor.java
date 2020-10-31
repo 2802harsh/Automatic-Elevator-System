64e7aca92edf28967cf6323537d6500b9d5a2ac9
insane1304 <iit2019124@iiita.ac.in>
2020-11-01 00:13:52 +0530

    Added variables and functions in floor class

--- src/backend/Floor.java
+++ src/backend/Floor.java
@@ -10,5 +10,27 @@ package backend;
  * @author DELL
  */
 public class Floor {
+    protected int currentFloor;
+    protected int targetFloor;
+    protected int intmdFloor;
     
+    public void setCurrentFloor(int floor)
+    {
+        currentFloor=floor;
+    }
+    public void setTargetFloor(int floor){
+        targetFloor=floor;
+    }
+    public void setIntmdFloor(int floor){
+        intmdFloor=floor;
+    }
+    public int getCurrentFloor(){
+        return currentFloor;
+    }
+    public int getTargetFloor(){
+        return targetFloor;
+    }
+    public int getIntmdFloor(){
+        return intmdFloor;
+    }
 }
